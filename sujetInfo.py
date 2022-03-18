fceb50fb7b21235a797c31e613fa8751d627e650
Thomas Florion <thomas.florion1@gmail.com>
2022-03-18 17:12:14 +0400

    ajout variables

--- sujetInfo.py
+++ sujetInfo.py
@@ -3,3 +3,14 @@ from scipy import linspace
 from scipy.signal import lti, step
 from matplotlib import pyplot as plt
 from random import random
+
+d0 = 1
+d1 = 1.13
+d2 = 0.273
+d3 = 0.0072
+K = 8
+
+numG = [K]
+denG = [d3,d2,d1,d0]
+
+G = lti(numG, denG)
