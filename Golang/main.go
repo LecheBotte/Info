198effe8c346c115828b4ecbd79250ac64ad6617
Thomas Florion <thomas.florion1@gmail.com>
2022-03-25 21:17:16 +0400

    hello world program

--- Golang/main.go
+++ Golang/main.go
@@ -1,3 +1,13 @@
 package main
 
-func main() {}
+import "fmt"
+
+/*
+
+go run ./main.go		execute le code
+go build ./main.go	construit le .exe du code
+
+*/
+func main() {
+	fmt.Printf("Hello world!")
+}
